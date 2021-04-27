70f6e18d87fc8c54e4b3ea208bf21bfaa666b8a5
mmanka1 <33776177+mmanka1@users.noreply.github.com>
2021-04-26 20:13:33 -0400

    Included assists, goals and npxG in cleaned data

--- backend/feature-selector.py
+++ backend/feature-selector.py
@@ -10,18 +10,22 @@ df_players.sort_values(by=['opponent'], inplace=True)
 #Drop non-numeric columns
 df_players.drop(['player', 'opponent'], axis=1, inplace=True)
 
+#Get features
+X = df_players.drop(['points'], axis=1)
+
 #Find highly correlated feature variables
 vif_data = pd.DataFrame() 
-vif_data["feature"] = df_players.columns 
-vif_data["VIF"] = [variance_inflation_factor(df_players.values, i) for i in range(len(df_players.columns))] 
+vif_data["feature"] = X.columns 
+vif_data["VIF"] = [variance_inflation_factor(X.values, i) for i in range(len(X.columns))] 
 print(vif_data)
 
-#Drop columns corresponding to higher VIFs and check new VIFs
-df_players.drop(['yellow_cards', 'red_cards', 'xGBuildup', 'assists', 'npxG', 'goals'], axis=1, inplace=True)
+#Drop columns corresponding to features with higher VIFs and check new VIFs
+X_cleaned = X.drop(['yellow_cards', 'red_cards', 'npG', 'xGBuildup'], axis=1)
 
+print('\nAfter removing features with high VIF:')
 vif_data_cleaned = pd.DataFrame() 
-vif_data_cleaned["feature"] = df_players.columns 
-vif_data_cleaned["VIF"] = [variance_inflation_factor(df_players.values, i) for i in range(len(df_players.columns))] 
+vif_data_cleaned["feature"] = X_cleaned.columns 
+vif_data_cleaned["VIF"] = [variance_inflation_factor(X_cleaned.values, i) for i in range(len(X_cleaned.columns))] 
 print(vif_data_cleaned)
 
 #Write cleaned dataframe to csv
