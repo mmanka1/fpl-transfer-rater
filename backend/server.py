685016c86a95eb67976d6585392f419ee3b91bf2
mmanka1 <33776177+mmanka1@users.noreply.github.com>
2021-05-05 11:01:30 -0400

    Added separate endpoint to encapsulate model training

--- backend/server.py
+++ backend/server.py
@@ -5,6 +5,7 @@ from userController import UserController
 from model import Model
 
 app = Flask(__name__)
+model = None
 
 def get_user(id, email, password):
     return UserController(user_id=id,email=email, password=password)
@@ -31,41 +32,60 @@ def my_team():
     response.headers["Content-Type"] = "application/json"
     return response
 
-@app.route('/rate', methods=['POST', 'GET'])
-def rater():
+@app.route('/train', methods=['POST', 'GET'])
+def train():
+    global model
     model = Model(retrain=True)
-    user = get_user(1, "", "")
+    response = make_response(
+        jsonify(
+            {"Message": "model trained successfully"}
+        )
+    )
+    response.headers["Content-Type"] = "application/json"
+    return response
 
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
+@app.route('/rate', methods=['GET'])
+def rater():
+    if model is not None:
+        user = get_user(1, "", "")
 
-    task = loop.create_task(user.set_user_transfer_status())
-    loop.run_until_complete(task)
-    bank = user.get_bank()
-    free_transfers = user.get_free_transfer_limit()
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
 
-    rater = TransferRater(predictor=model.get_model(), free_transfers=free_transfers, player_out="Harry Kane", player_target="Chris Wood", next_gws=4, starting_gw=0)
-    r = rater.get_feedback(bank=bank, selling_price=10.2)
-    feedback = r[0]
-    rating = r[1]
-    points_in = rater.get_expected_points_in()
-    points_out = rater.get_expected_points_out()
-    risk_reward_ratio = rater.get_risk_reward()
-    chance_playing_transfer_in = rater.get_chance_playing_in()
-    chance_playing_transfer_out = rater.get_chance_playing_out()
+        task = loop.create_task(user.set_user_transfer_status())
+        loop.run_until_complete(task)
+        bank = user.get_bank()
+        free_transfers = user.get_free_transfer_limit()
+
+        rater = TransferRater(predictor=model.get_model(), free_transfers=free_transfers, player_out="Harry Kane", player_target="Chris Wood", next_gws=4, starting_gw=0)
+        r = rater.get_feedback(bank=bank, selling_price=10.2)
+        feedback = r[0]
+        rating = r[1]
+        points_in = rater.get_expected_points_in()
+        points_out = rater.get_expected_points_out()
+        risk_reward_ratio = rater.get_risk_reward()
+        chance_playing_transfer_in = rater.get_chance_playing_in()
+        chance_playing_transfer_out = rater.get_chance_playing_out()
+
+        response = make_response(
+            jsonify(
+                {
+                    "feedback": feedback,
+                    "transfer_rating": rating,
+                    "points_transfer_in": str(points_in),
+                    "points_transfer_out": str(points_out),
+                    "risk_reward_ratio": risk_reward_ratio,
+                    "chance_playing_transfer_in": chance_playing_transfer_in,
+                    "chance_playing_transfer_out": chance_playing_transfer_out,
+                }
+            )
+        )
+        response.headers["Content-Type"] = "application/json"
+        return response
 
     response = make_response(
         jsonify(
-            {
-                "feedback": feedback,
-                "transfer_rating": rating,
-                "points_gain": str(points_in - points_out),
-                "points_transfer_in": str(points_in),
-                "points_transfer_out": str(points_out),
-                "risk_reward_ratio": str(risk_reward_ratio),
-                "chance_playing_transfer_in": chance_playing_transfer_in,
-                "chance_playing_transfer_out": chance_playing_transfer_out,
-            }
+            {"error": "model has not been trained"}
         )
     )
     response.headers["Content-Type"] = "application/json"
