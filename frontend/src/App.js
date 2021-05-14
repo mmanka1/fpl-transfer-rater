120090bfae58301171c4e797bbe34fb6b4d8f392
mmanka1 <33776177+mmanka1@users.noreply.github.com>
2021-05-14 15:26:36 -0400

    Added frontend react app

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import PortalView from './components/portalView'; 
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <PortalView/>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/portalView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const PortalView = () => {
+    return (
+        <div>
+            <h3>Find my team</h3>
+            <input name = "id" placeholder = "Enter id"/>
+        </div>
+    )
+}
+
+export default PortalView
